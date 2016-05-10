ac0ed6f095a11609a3329db190039a7a3512c836
nov0 <novose@gmail.com>
2016-05-10 10:57:51 +0200

    initial commit

---
+++ src/main/java/org/bildit/lingua/BildItLinguaApplication.java
@@ -0,0 +1,12 @@
+package org.bildit.lingua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildItLinguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BildItLinguaApplication.class, args);
+	}
+}
