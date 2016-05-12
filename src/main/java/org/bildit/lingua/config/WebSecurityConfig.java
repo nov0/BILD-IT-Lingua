2e869c73c127832d73c3b1df5435d98c95776331
beyon <bojan.aleksich@gmail.com>
2016-05-12 14:03:36 +0200

    test

--- src/main/java/org/bildit/lingua/config/WebSecurityConfig.java
+++ src/main/java/org/bildit/lingua/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package org.bildit.lingua.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,14 +11,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/").permitAll()
-		.antMatchers("/logout").permitAll()
-		.antMatchers("/jdbc").permitAll()
-		.antMatchers("/get-users").permitAll()
-		.antMatchers("/user-account").permitAll()
-		.anyRequest().denyAll()
+			.antMatchers("/").permitAll()
+			.antMatchers("/logout").permitAll()
+			.antMatchers("/jdbc").permitAll()
+			.antMatchers("/get-users").permitAll()
+			.antMatchers("/user-account").permitAll()
+			.anyRequest().denyAll()
 		.and()
-		.formLogin().and().logout();
+			.formLogin()
+		.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 	}
 	
 }
