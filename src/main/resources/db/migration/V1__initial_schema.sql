642b5bc54d7c6bbc999d2503e817d8ba22da2b2a
djomla79 <tm.djomla79@gmail.com>
2016-05-15 17:40:51 +0200

    Unnecessery files deleted, some new added

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE `base_users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `authority` varchar(255) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
-  `enabled` tinyint(1) DEFAULT NULL,
+  `enabled` bit(1) DEFAULT NULL,
   `first_name` varchar(25) DEFAULT NULL,
   `last_name` varchar(25) DEFAULT NULL,
   `password` varchar(80) NOT NULL,
@@ -21,3 +21,17 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`id`),
   CONSTRAINT `FK_2d37nsdu6wfjfoo0q1l857edx` FOREIGN KEY (`id`) REFERENCES `base_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `adding_ban` bit(1) DEFAULT NULL,
+  `login_ban` bit(1) DEFAULT NULL,
+  `voting_ban` bit(1) DEFAULT NULL,
+  `id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_8qtpnv06elxuryeuv1ac4ximm` FOREIGN KEY (`id`) REFERENCES `base_users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
