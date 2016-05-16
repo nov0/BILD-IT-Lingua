90069f08b16937c6028e39bde0156917d602732e
beyon <bojan.aleksich@gmail.com>
2016-05-16 21:40:42 +0200

    locale for languages solved

--- src/main/resources/static/js/language-btn.js
+++ src/main/resources/static/js/language-btn.js
@@ -1,13 +1,23 @@
-var lang = location.search;
+/**
+ * @Author Bojan Aleksic
+ * Language Internationalization
+ * Change language within drop-down menu, using JS and
+ * locale from cookies
+ */
+
+var lang = "";
 var flag = "";
-if(lang === "?lang=en") {
-	lang = "English";
-	flag = "images/eng-flag.png";
-} else if(lang === "?lang=sr") {
+
+if(document.cookie.indexOf("LOCALE=sr") > -1) {
 	lang = "Serbian";
 	flag = "images/serbia-flag.png";
-} else if(lang !== "?lang=sr" || lang !== "?lang=en") {
-	lang = "";
+} else if(document.cookie.indexOf("LOCALE=en") > -1) {
+	lang = "English";
+	flag = "images/eng-flag.png";
+} else if(document.cookie.indexOf("LOCALE=en") < 0 || document.cookie.indexOf("LOCALE=sr") < 0) {
+	lang = "English";
+	flag = "images/eng-flag.png";
 }
+
 $("#chosen-lang").css({"background": "url('" + flag + "') no-repeat", "background-position": "-2px 2px"});
 $("#chosen-lang").text(lang);
