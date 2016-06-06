4a64d737e2fb850228737eb3c56955b8492457f7
gonguli <goran.988@hotmail.com>
2016-06-06 11:31:17 +0200

    controller edited, now test work fine

--- src/main/java/org/bildit/lingua/controllers/PracticeController.java
+++ src/main/java/org/bildit/lingua/controllers/PracticeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PracticeController {
 
+	
+	//TODO: Now is just for test form submit, must be changed
 	@RequestMapping(value="/practice", method=RequestMethod.POST)
 	public String startPractice(
 			@RequestParam("from") String from,
@@ -16,6 +18,6 @@ public class PracticeController {
 			@RequestParam("order") String order) {
 		
 		System.out.println(from + " " + category + " " + speed + " " + order);
-		return "home";
+		return "error";
 	}
 }
