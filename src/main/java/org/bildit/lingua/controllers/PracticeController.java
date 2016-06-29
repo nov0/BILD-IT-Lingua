bbdb40ce14024f18a0a2cd23e6d5552b9b0403aa
beyon <bojan.aleksich@gmail.com>
2016-06-29 04:38:54 +0200

    Resetting Stack List if user leave the practice - DONE.

--- src/main/java/org/bildit/lingua/controllers/PracticeController.java
+++ src/main/java/org/bildit/lingua/controllers/PracticeController.java
@@ -10,10 +10,12 @@ import org.bildit.lingua.service.PracticeService;
 import org.bildit.lingua.service.TicketService;
 import org.bildit.lingua.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -122,7 +124,13 @@ public class PracticeController {
 		return true;
 	}
 	
+	/**
+	 * @author Bojan Aleksic
+	 * Method clears out the Stack, if user leave practice 
+	 * before it ends
+	 */
 	@RequestMapping("/reset-practice")
+	@ResponseStatus(value = HttpStatus.OK)
 	public void clearStackOnPracticeAbort() {
 		if(!stack.isEmpty()) {
 			stack.clear();
