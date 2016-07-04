42c83ff4ffcd09808c5c33282a3806e36c661873
djomla79 <tm.djomla79@gmail.com>
2016-07-04 18:20:23 +0200

    Unicode UTF-8 enabled

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -5,15 +5,15 @@ DROP TABLE IF EXISTS `base_users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `base_users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `authority` varchar(255) DEFAULT NULL,
-  `email` varchar(80) NOT NULL,
-  `enabled` bit(1) DEFAULT NULL,
-  `first_name` varchar(25) NOT NULL,
-  `last_name` varchar(25) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `username` varchar(25) NOT NULL,
+  `authority` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` bit(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `first_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `languages`;
@@ -21,9 +21,9 @@ DROP TABLE IF EXISTS `languages`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `languages` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `language_title` varchar(255) DEFAULT NULL,
+  `language_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `user`;
@@ -42,7 +42,7 @@ CREATE TABLE `user` (
   CONSTRAINT `FK_8qtpnv06elxuryeuv1ac4ximm` FOREIGN KEY (`id`) REFERENCES `base_users` (`id`),
   CONSTRAINT `FK_b06c1ydioahlapib3ttlgb0jk` FOREIGN KEY (`foreign_language`) REFERENCES `languages` (`id`),
   CONSTRAINT `FK_hi3yehoowqynwt5f5rjntcb91` FOREIGN KEY (`domestic_language`) REFERENCES `languages` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `tickets`;
@@ -50,12 +50,12 @@ DROP TABLE IF EXISTS `tickets`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tickets` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category` varchar(255) DEFAULT NULL,
-  `date_created` varchar(255) DEFAULT NULL,
+  `category` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_created` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `deactivated` date DEFAULT NULL,
   `edited` bit(1) DEFAULT NULL,
-  `text_domestic` varchar(255) DEFAULT NULL,
-  `text_foreign` varchar(255) DEFAULT NULL,
+  `text_domestic` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `text_foreign` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `learning_language` bigint(20) DEFAULT NULL,
   `user` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -63,7 +63,7 @@ CREATE TABLE `tickets` (
   KEY `FK_1o1qks9jxgqgnoqnyduycqlb8` (`user`),
   CONSTRAINT `FK_1o1qks9jxgqgnoqnyduycqlb8` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
   CONSTRAINT `FK_7l33mxamukg6om3h7yhtn5ptn` FOREIGN KEY (`learning_language`) REFERENCES `languages` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `votes`;
@@ -77,7 +77,7 @@ CREATE TABLE `votes` (
   PRIMARY KEY (`id`),
   KEY `FK_f78dg7c8e7y7ne8cfnevgd869` (`ticket`),
   CONSTRAINT `FK_f78dg7c8e7y7ne8cfnevgd869` FOREIGN KEY (`ticket`) REFERENCES `tickets` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `votes_users`;
@@ -90,7 +90,7 @@ CREATE TABLE `votes_users` (
   KEY `UK_luhu2ggyumag1y7t6jhpg8ion` (`user_id`),
   CONSTRAINT `FK_cxa7y2u2v1sak5idgjm325hb` FOREIGN KEY (`vote_id`) REFERENCES `votes` (`id`),
   CONSTRAINT `FK_luhu2ggyumag1y7t6jhpg8ion` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `user_tickets`;
@@ -103,5 +103,5 @@ CREATE TABLE `user_tickets` (
   KEY `FK_c1ltcs2jv8b75ts4ukncb3frh` (`user_id`),
   CONSTRAINT `FK_9bh6c1lny71j16qsav1gt0eao` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`),
   CONSTRAINT `FK_c1ltcs2jv8b75ts4ukncb3frh` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
