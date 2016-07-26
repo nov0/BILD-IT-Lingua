8b7fbce2d2590b73ed27d2dd3a035323de40b36b
beyon <bojan.aleksich@gmail.com>
2016-07-26 10:47:43 +0200

    iText for rendering PDF files has been implemented

---
+++ src/main/java/org/bildit/lingua/controllers/ReportController.java
@@ -0,0 +1,80 @@
+package org.bildit.lingua.controllers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bildit.lingua.model.Ticket;
+import org.bildit.lingua.pdf.helper.GeneratePdf;
+import org.bildit.lingua.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ReportController {
+	
+	@Autowired
+	private ReportService reportService;
+
+	/**
+	 * @author Bojan Aleksic
+	 * @param request
+	 * @param response
+	 * @param downloadRequest
+	 * @param languageRequest
+	 * @throws IOException
+	 */
+	@RequestMapping(value="/download")
+	public void downloadPdf(HttpServletRequest request, 
+			HttpServletResponse response, 
+			@RequestParam("request") String downloadRequest, 
+			@RequestParam(value="languageRequest", required=false) String languageRequest) throws IOException {
+		
+		languageRequest = "English"; // dok se ne popravi jezik u reports.html-u
+		
+		List<Ticket> records = null;
+		
+		String fileName = "";
+		
+		if("top-users".equals(downloadRequest)) {
+			fileName = "Top 20 Users Ordered by Reputation.pdf";
+			// implement top users here
+		} else if("top-entries".equals(downloadRequest)) {
+			fileName = "Top 20 entries for selected language based on reputation.pdf";
+			records = reportService.getTopEntries(languageRequest);
+		} else if("banned-users".equals(downloadRequest)) {
+			fileName = "Banned users.pdf";
+			// implement banned users here
+		} else if("statistic".equals(downloadRequest)) {
+			fileName = "General statistic of application.pdf";
+			// implement statistic here
+		}
+		
+		final ServletContext servletContext = request.getSession().getServletContext();
+		final File tempDirectory = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+		final String tempFilePath = tempDirectory.getAbsolutePath();
+		
+		response.setContentType("application/pdf");
+		response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+		
+		try {
+			GeneratePdf.createPdf(tempFilePath + "\\" + fileName, downloadRequest, records);
+			ByteArrayOutputStream byteOutStream = GeneratePdf.convertPdfToByteArrayOutputStream(tempFilePath + "\\" + fileName);
+			OutputStream outputStream = response.getOutputStream();
+			byteOutStream.writeTo(outputStream);
+			outputStream.flush();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+	
+}
--- src/main/java/org/bildit/lingua/service/ReportServiceImpl.java
+++ src/main/java/org/bildit/lingua/service/ReportServiceImpl.java
@@ -1,52 +1,25 @@
 package org.bildit.lingua.service;
 
-import java.io.IOException;
 import java.util.List;
 
-import org.bildit.lingua.model.User;
+import org.bildit.lingua.model.Ticket;
+import org.bildit.lingua.repository.LanguageRepository;
+import org.bildit.lingua.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.itextpdf.io.font.FontConstants;
-import com.itextpdf.kernel.font.PdfFont;
-import com.itextpdf.kernel.font.PdfFontFactory;
-import com.itextpdf.kernel.geom.PageSize;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Cell;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
 
 @Service
 public class ReportServiceImpl implements ReportService {
+	
+	@Autowired
+	private TicketRepository ticketRepository;
+	
+	@Autowired
+	private LanguageRepository languageRepository;
 
-	public void downloadPdf(List<User> list, String title) throws IOException {
-
-		PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
-
-		PdfWriter writer = new PdfWriter("destination");
-		PdfDocument pdf = new PdfDocument(writer);
-		Document document = new Document(pdf, PageSize.A4);
-		Table table = new Table(new float[] { 4, 4, 4, 1, 1, 4, 2, 2, 2 });
-		table.setWidthPercent(100);
-		document.add(new Paragraph(title).setFont(font));
-		for (User user : list) {
-			proces(table, user);
-		}
-		document.add(table);
-		document.close();
+	@Override
+	public List<Ticket> getTopEntries(String languageRequest) {
+		return ticketRepository.findAllByLearningLanguage(languageRepository.getOneByLanguageTitle(languageRequest));
 	}
 	
-	private void proces(Table table, User user) {
-		table.addCell(new Cell().add(new Paragraph(user.getFirstName())));
-		table.addCell(new Cell().add(new Paragraph(user.getLastName())));
-		table.addCell(new Cell().add(new Paragraph(user.getUsername())));
-		table.addCell(new Cell().add(new Paragraph("like")));
-		table.addCell(new Cell().add(new Paragraph("dislike")));
-		table.addCell(new Cell().add(new Paragraph("total number of tickets")));
-		table.addCell(new Cell().add(new Paragraph(Boolean.toString(user.isAddingBan()))));
-		table.addCell(new Cell().add(new Paragraph(Boolean.toString(user.isEnabled()))));
-		table.addCell(new Cell().add(new Paragraph(Boolean.toString(user.isVotingBan()))));
-	}
-
 }
